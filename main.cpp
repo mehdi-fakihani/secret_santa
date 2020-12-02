af436836ac90175df7853956ae23a87da9e6eb4b
mehdi-fakihani <mehdi.fakihani@outlook.com>
2020-12-03 00:36:51 +0100

    Version 1.0
    The participants are inputed through "participants.txt".
    This file must be next to the executable.
    There must be one participant name on each line.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World !" << std::endl;
+    std::ifstream participantsFile{"participants.txt"};
+    std::string participant;
+    std::vector<std::string> participants;
+
+    std::random_device random_device;
+    std::mt19937 random_number_generator{random_device()};
+
+    while(participantsFile >> participant)
+        participants.emplace_back(std::move(participant));
+
+    std::vector partners{participants};
+
+    std::shuffle(partners.begin(), partners.end(), random_number_generator);
+
+    for(std::size_t i = 0; i < participants.size(); ++i)
+        if(participants[i] == partners[i])
+            std::rotate(partners.rbegin(), partners.rbegin() + 1, partners.rend());
+
+    for(std::size_t i = 0; i < participants.size(); ++i)
+        std::cout << "Couple #" << i << ": " << participants[i] << " " << partners[i] << std::endl;
 }
